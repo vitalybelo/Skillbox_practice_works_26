09f31b08ca5798233ae19b6457bfc6d3a0819545
vitaly beloserkovskii <vitaly.belo@gmail.com>
2022-11-14 12:12:08 +0300

    Initial commit

---
+++ include/use_lib.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <iostream>
+#include <random>
+#include "windows.h"
+using namespace std;
+
+
+string getCommand() {
+    string command;
+    getline(cin,command);
+    for (char & i : command) {
+        i = (char) tolower(i);
+    }
+    return command;
+}
+
+void clearStdin () {
+    cin.clear();
+    fflush(stdin);
+}
+void setlocaleRus () {
+    setlocale(LC_ALL, "Russian.Russia");
+}
+
+void setlocale1251 () {
+    setlocaleRus();
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+}
+
+int random_nextInt (const int from, const int to) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> dist(from,to);
+
+    return dist(gen);
+    //return (int)gen();
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "use_lib.h"
+using namespace std;
+
+int main() {
+    setlocale1251();
+
+
+    return 0;
+}
